fdaf40e87b0722ee3c1649cc542d7c4d4c4d9496
Sabarish1241 <sabarishcloud410@gmail.com>
2024-03-19 19:27:09 +0530

    adding step to remove old content

--- backend.sh
+++ backend.sh
@@ -3,7 +3,7 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
 cp backend.service /etc/systemd/system/backend.service
-
+rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
